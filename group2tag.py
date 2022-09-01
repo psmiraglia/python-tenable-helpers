04ada62ed52af7a43f7791ad1b4a3924b84f1f57
Paolo Smiraglia <paolo.smiraglia@gmail.com>
2022-09-01 16:45:09 +0200

    Fetch agents and assets using pagination

--- group2tag.py
+++ group2tag.py
@@ -19,11 +19,70 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
+import json
+
 import click
 from tenable.io import TenableIO
 
 import commons
 from keys import ACCESS_KEY, SECRET_KEY
+
+
+def _get_agents(tio, g_id, limit, offset):
+    res = tio.agent_groups.details(g_id, limit=limit, offset=offset)
+    tot = res.get('pagination').get('total')
+    agents = [a for a in res.get('agents')]
+    return agents, tot
+
+
+def _get_assets(tio, agents):
+    limit = 100
+    assets = []
+    f = {
+        'and': [
+            {
+                'property': 'host_name',
+                'operator': 'eq',
+                'value': [a.get('name') for a in agents]
+            },
+            {
+                'property': 'sources',
+                'operator': 'eq',
+                'value': ['NESSUS_AGENT']
+            }
+        ]
+    }
+
+    # first page
+    res = tio.v3.explore.assets.search_all(filter=f,
+                                           limit=limit,
+                                           return_resp=True)
+    data = json.loads(res.text)
+
+    _assets = [a for a in data.get('assets')]
+    if len(_assets) > 0:
+        assets += _assets
+        print(f'(*) Got {len(assets)} assets')
+    token = data.get('pagination').get('next')
+
+    # other pages
+    while token:
+        res = tio.v3.explore.assets.search_all(filter=f, limit=limit, next=token, return_resp=True)  # noqa
+        data = json.loads(res.text)
+
+        _assets = [a for a in data.get('assets')]
+        if len(_assets) > 0:
+            assets += _assets
+            print(f'Got {len(assets)} assets')
+        token = data.get('pagination').get('next')
+
+    # return assets
+    return assets
+
+
+def _assign_tag(tio, tag_id, tag_name, assets):
+    tio.tags.assign([a.get('id') for a in assets], [tag_id])
+    print(f'(*) Tag "{tag_name}" has been assigned to {"|".join([a.get("name") for a in assets])}')  # noqa
 
 
 @click.command()
@@ -42,38 +101,35 @@ def g2t(g_name, g_id):
         g_name = g.get('name')
         g_id = g.get('id')
 
-    # get agents from group
-    _agents = tio.agent_groups.details(g_id)['agents']
-    agents = [a for a in _agents]
-
-    # get assets from agents
-    _assets = tio.v3.explore.assets.search_all(filter={
-        'and': [
-            {
-                'property': 'host_name',
-                'operator': 'eq',
-                'value': [a.get('name') for a in agents]
-            },
-            {
-                'property': 'sources',
-                'operator': 'eq',
-                'value': ['NESSUS_AGENT']
-            }
-        ]
-    })
-    assets = [a for a in _assets]
-
     # create tag
     tag = commons.create_tag(tio, 'AgentGroup', g_name, True)
     tag_name = f'{tag.get("category_name")}:{tag.get("value")}'
     tag_id = tag.get('uuid')
     print(f'(*) Tag "{tag_name}" with ID "{tag_id}" has been created')
 
-    # assign tag
-    tio.tags.assign([a.get('id') for a in assets], [tag_id])
-    print(f'(*) Tag "{tag_name}" has been assigned to...')
-    for name in [a.get('name') for a in assets]:
-        print(f'(+) {name}')
+    #
+    # iterate over agents in agent group
+    #
+    agents = []
+    offset = 0
+    limit = 100
+    got = 0
+
+    # first page
+    agents, tot = _get_agents(tio, g_id, limit, offset)
+    got += len(agents)
+    print(f'(*) Got {got} over {tot} agents')
+    assets = _get_assets(tio, agents)
+    _assign_tag(tio, tag_id, tag_name, assets)
+
+    # other pages
+    while got < tot:
+        offset += limit
+        agents, tot = _get_agents(tio, g_id, limit, offset)
+        got += len(agents)
+        print(f'(*) Got {got} over {tot} agents')
+        assets = _get_assets(tio, agents)
+        _assign_tag(tio, tag_id, tag_name, assets)
 
 
 if __name__ == '__main__':
